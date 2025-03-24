c3a27e18d49c33612cfe525d359c9be0f0937d2d
Rex Fernando <rex1fernando@gmail.com>
2025-03-24 17:29:47 +0000

    Automate mocking of on-chain config for test deployment of prover service image

---
+++ test/mock-on-chain/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd prover-service
+python3 -m http.server 4444
---
+++ test/test_docker_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "" > ./test/testing_envvars.txt
+echo "ONCHAIN_GROTH16_VK_URL=http://mock-on-chain:4444/groth16_vk.json" >> ./test/testing_envvars.txt
+echo "ONCHAIN_TW_VK_URL=http://mock-on-chain:4444/keyless_config.json" >> ./test/testing_envvars.txt
+echo "PRIVATE_KEY_0=$(cat ./prover-service/private_key_for_testing.txt)" >> ./test/testing_envvars.txt
+echo "PRIVATE_KEY_1=$(cat ./prover-service/private_key_for_testing_another.txt)" >> ./test/testing_envvars.txt
+
+pushd prover-service
+LOCAL_VK_IN=~/.local/share/aptos-prover-service/default/verification_key.json ONCHAIN_VK_OUT=groth16_vk.json cargo test groth16_vk_rewriter
+LOCAL_TW_VK_IN=private_key_for_testing.txt ONCHAIN_KEYLESS_CONFIG_OUT=keyless_config.json cargo test tw_vk_rewriter
+popd
+
+sudo docker compose -f ./test/test_deployment.yml up
+
+
+
+
