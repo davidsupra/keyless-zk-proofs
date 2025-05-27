64ffb8324b722932fc7cb4c583aa0c60cf31af56
Alin Tomescu <tomescu.alin@gmail.com>
2025-05-26 23:49:21 -0400

    move circom tests/ outside of templates/ directory

--- circuit/src/lib.rs
+++ circuit/src/lib.rs
@@ -39,8 +39,7 @@ impl TestCircuitHandle {
     /// Compile the circuit in the given file using BN254 as the underlying curve.
     pub fn new(file_name: &str) -> anyhow::Result<Self> {
         let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let include_root_dir = cargo_manifest_dir.join("./templates");
-        let src_circuit_path = include_root_dir.join("tests").join(file_name);
+        let src_circuit_path = cargo_manifest_dir.join("tests").join(file_name);
         let content = fs::read_to_string(src_circuit_path)?;
         Self::new_from_str(content.as_str())
     }
